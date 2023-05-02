eb51164e3cefa86a68b595a78ac8c701559f18e4
Jakub Nowak <86384726+ham222@users.noreply.github.com>
2023-05-02 21:36:32 +0200

    Proper initial

---
+++ src/main/java/ads/handins/QueensSolver.java
@@ -0,0 +1,10 @@
+package ads.handins;
+
+public class QueensSolver{
+
+    public static boolean[][] solveProblem(int size){
+        boolean[][] arr = new boolean[size][size];
+        
+        return arr;
+    }
+}
